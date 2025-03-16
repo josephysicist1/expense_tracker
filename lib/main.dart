6b530c395cffc74b31b9d09d9891f2b61fd2c797
Yusuf Gök <gok.yusuf@protonmail.com>
2025-03-16 18:28:14 +0300

    themesAdjusted

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,47 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark,
+);
 void main() {
   runApp(
     MaterialApp(
       home: Expenses(),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+      ),
       theme: ThemeData(
+        // buradaki light mode theme
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16),
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
             backgroundColor: kColorScheme.onPrimaryContainer,
             foregroundColor: kColorScheme.primaryContainer,
             centerTitle: true),
       ),
+      themeMode: ThemeMode.dark, // theme mode'unu sisteme bırakıyoruz
     ),
   );
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -31,3 +31,15 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    return sum;
+  }
+}
